6a217ce88ae773b13b577a6205f283b73939d3aa
Klorpk <arsenijgorozankin62@gmail.com>
2025-12-30 16:37:53 +0300

    Add files via upload

---
+++ Task7.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args){
+        var scn = new Scanner(System.in);
+        var n= scn.nextInt();
+        Random random = new Random();
+        int[] array = new int[n];
+        for ( int i=0;i < n;i++) {
+            array[i] = random.nextInt(-100,100);
+        }
+        int[] array1 = new int[n];
+        for (int j=0;j<n;j++){
+            if (j%2==0){
+                if(array[j]<0) {
+                    array1[j] = array[j];
+                    System.out.println(j);
+                    break;
+                }
+            }
+        }
+        Arrays.sort(array1);
+        if(array1[0]>=0){
+            System.out.println(-1);
+        }
+        System.out.println(Arrays.toString(array));
+
+    }
+}
